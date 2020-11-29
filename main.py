44908a47a47f293c82ef591b3267a3dd5cd4a448
francisco-quintana <francisco.quintanas12@gmail.com>
2020-11-29 10:04:23 -0700

    heroku

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ users.append(User(id=4, username='Melissa', password='123'))
 users.append(User(id=5, username='Komaba', password='123'))
 users.append(User(id=6, username='Main', password='1234'))
 
-app = create_app()
+app = Flask(__name__)
 
 @app.before_request
 def before_request():
